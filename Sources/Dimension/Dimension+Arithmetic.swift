9ad6a6c92e97d49c59b2a556443afb4d7882998d
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-28 22:58:39 +0200

    Restore intrinsic Scale arithmetic

--- Sources/Dimension/Dimension+Arithmetic.swift
+++ Sources/Dimension/Dimension+Arithmetic.swift
@@ -1,3 +1,42 @@
+@inlinable
+public func * <Scalar: FloatingPoint>(
+    lhs: Scale<1, Scalar>,
+    rhs: Scale<1, Scalar>
+) -> Scale<1, Scalar> {
+    Scale(lhs.value * rhs.value)
+}
+
+@inlinable
+public func / <Scalar: FloatingPoint>(
+    lhs: Scale<1, Scalar>,
+    rhs: Scale<1, Scalar>
+) -> Scale<1, Scalar> {
+    Scale(lhs.value / rhs.value)
+}
+
+@inlinable
+public func + <Scalar: FloatingPoint>(
+    lhs: Scale<1, Scalar>,
+    rhs: Scale<1, Scalar>
+) -> Scale<1, Scalar> {
+    Scale(lhs.value + rhs.value)
+}
+
+@inlinable
+public func - <Scalar: FloatingPoint>(
+    lhs: Scale<1, Scalar>,
+    rhs: Scale<1, Scalar>
+) -> Scale<1, Scalar> {
+    Scale(lhs.value - rhs.value)
+}
+
+@inlinable
+public func sqrt<Scalar: FloatingPoint>(
+    _ value: Scale<1, Scalar>
+) -> Scale<1, Scalar> {
+    Scale(value.value.squareRoot())
+}
+
 extension Scale where Scalar: SignedNumeric & FloatingPoint {
 
     @inlinable
@@ -8,4 +47,11 @@ extension Scale where Scalar: SignedNumeric & FloatingPoint {
         }
         return Self(result)
     }
+}
+
+@inlinable
+public func abs<Scalar: FloatingPoint & Comparable>(
+    _ value: Scale<1, Scalar>
+) -> Scale<1, Scalar> {
+    value.value < 0 ? Scale(-value.value) : value
 }
